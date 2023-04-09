77d2e89441f42b592ae2113407599a872654a581
minhN2000 <nguye3mt@mail.uc.edu>
2023-04-08 21:13:16 -0400

    Update gitHubIssueRetrieval.py

--- GitHubRetrieval/gitHubIssueRetrieval.py
+++ GitHubRetrieval/gitHubIssueRetrieval.py
@@ -3,7 +3,7 @@ import time
 import pandas as pd
 import json
 import argparse
-
+import re
 parser = argparse.ArgumentParser(description='Script to retrieve issues data for GitHub repositories')
 parser.add_argument('--filePath', '-f', type=str, help='Path to the CSV file containing the list of repositories')
 parser.add_argument('--restToken', '-r', type=str, default="",
@@ -11,7 +11,7 @@ parser.add_argument('--restToken', '-r', type=str, default="",
 parser.add_argument('--graphQLToken', '-g', type=str,default="",
                     help='The GitHub GraphQL API token, needed to improve query rate limit')
 
-def getQuery(userName, reposName, issueNumber):
+def getQuery(userName, reposName, issueNum):
     '''
     This function returns a GraphQL query that  retrieves information 
     about a GitHub issue given the issue number, repository name, and repository owner.
@@ -19,15 +19,18 @@ def getQuery(userName, reposName, issueNumber):
     return f'''
     query {{
         repository(owner: "{userName}", name: "{reposName}") {{
-            issue(number: {issueNumber}) {{
+            issue(number: {issueNum}) {{
+                participants {{
+                    totalCount
+                }}
                 title
                 url
                 createdAt
                 author {{
                     login
                 }}
-                closed
                 body
+                closed
                 labels(first: 10) {{
                     edges {{
                         node {{
@@ -35,12 +38,9 @@ def getQuery(userName, reposName, issueNumber):
                         }}
                     }}
                 }}
-                comments(first: 100) {{
+                comments(first: 30) {{
                     edges {{
                         node {{
-                            author {{
-                                login
-                            }}
                             body
                         }}
                     }}
@@ -50,164 +50,92 @@ def getQuery(userName, reposName, issueNumber):
     }}
     '''
 
+def toTXT(reposName, issueNum, issueTitle, description, cleanedComments):
+    '''
+    This function convert the original issue to a simple version that is easy to read
+    '''
+    file = open(f'./recdroid_issues/appID_{reposName}_issueID_{issueNum}.txt', 'w')
+    temp_str = ("######Title##########\n")
+    temp_str += (issueTitle)
+    temp_str += ("\n######End Title##########\n")
+    temp_str += ("\n######Description##########\n")
+    temp_str += (description)
+    temp_str += ("\n######End Description##########\n")
+    temp_str += cleanedComments
+    temp_str = re.sub(r'\n\s*\n', '\n', temp_str)
+    file.write(temp_str)
+    file.close()
 
-def issuesRetrieval(userName, reposName, lastIssueNum, graphQLToken):
+
+def __getIssueInfo(issueUrl):
+    cleanUrl = issueUrl.replace('https://github.com/','')
+    infoList = cleanUrl.split('/')
+    userName, reposName, issueNum = infoList[0], infoList[1], infoList[3]
+    return userName, reposName, issueNum
+
+def issuesRetrieval(issueUrl, issuesList):
     '''
     This function uses the getQuery() function to retrieve information about all the issues
     in the given repository, starting from the first issue up to and including the last issue.
     '''
-    currentIssueNum = 0
-    graphQLHeader = {
-        'Authorization': f'Bearer {graphQLToken}'
-    }
-
+    if 'github.com' not in issueUrl:
+        log_file = open('Recdoid_log.txt', 'a')
+        error_str = f'{issueUrl} not from github\n'
+        print(error_str)
+        log_file.write(error_str)
+        log_file.close()
+        return
     graphQLurl = 'https://api.github.com/graphql'
-    issuesList = []
-    while currentIssueNum <= int(lastIssueNum):
-        # Retrieve the GraphQL query for the current issue
-        query = getQuery(userName, reposName, currentIssueNum)
-        # Post the query to the GitHub API
-        response = requests.post(
-            graphQLurl, json={'query': query}, headers=graphQLHeader)
-        try:
-            if response.status_code == 200:
-                # Parse the JSON data
-                data = json.loads(response.text)
-                # Check for errors in the response data
-                if 'errors' in data or data is None or data['data'] is None or data['data']['repository'] is None or data['data']['repository']['issue'] is None:
-                    if 'errors' in data:
-                        print(
-                            f'App {userName}/{reposName} Error at {currentIssueNum}: ' + str(data['errors'][0]['type']))
-                        if str(data['errors'][0]['type']) == 'RATE_LIMITED':
-                            print("write to .csv file")
-                            df = pd.DataFrame(issuesList, columns=['App Name', 'Issue Title', 'Issue Link', 'Date Created', 'User Created',
-                                                                   'Status', 'labels', 'Description', 'Comments', 'Images In Description',
-                                                                   'Images In Comments', 'Total Images', 'Total Participants'])
-                            df.to_csv(f'{reposName} issues.csv', index=False)
-                            time.sleep(3600)
-                        elif str(data['errors'][0]['type']) != 'NOT_FOUND':
-                            print(
-                                f"App {userName}/{reposName} Issue #{currentIssueNum} does not exist")
-                        else:
-                            currentIssueNum += 1
-                            continue
+    graphQLToken = 'YOUR_TOKEN_HERE'
+    graphQLHeader = {
+            'Authorization': f'Bearer {graphQLToken}'
+        }
 
-                else:
-                    # Extract the issue data from the response
-                    issueData = data['data']['repository']['issue']
+    userName, reposName, issueNum = __getIssueInfo(issueUrl)
 
-                    # Extract the data fields from the issue
-                    appName = str(reposName)
-                    issueTitle = issueData['title']
-                    issueLink = issueData['url']
-                    dateCreated = issueData['createdAt']
-                    if issueData['author'] is None:
-                        userCreated = "Unknown"
-                    else:
-                        userCreated = issueData['author']['login']
-                    status = 'Closed' if issueData['closed'] else 'Open'
-                    description = [issueData['body']]
+    q = getQuery(userName, reposName, issueNum)
+    response = requests.post(graphQLurl, json={'query': q}, headers=graphQLHeader)
 
-                    comments = issueData['comments']['edges']
-                    imagesInDescription = description.count(
-                        '.png') + description.count('.jpg') + description.count('.jpeg')
-                    imagesInComments = sum(comment['node']['body'].count('.png') + comment['node']['body'].count(
-                        '.jpg') + comment['node']['body'].count('.jpeg') for comment in comments)
-                    totalImages = imagesInDescription + imagesInComments
-                    participantNumber = set()
+    # try:
+    data = json.loads(response.text)
+    issueData = data['data']['repository']['issue']
+    issueTitle = issueData['title']
+    issueLink = issueData['url']
+    dateCreated = issueData['createdAt']
+    if issueData['author'] is None:
+                    userCreated = "Unknown"
+    else:
+        userCreated = issueData['author']['login']
+    status = 'Closed' if issueData['closed'] else 'Open'
+    comments = issueData['comments']['edges']
+    description = str(issueData['body'])
+    imagesInDescription = description.count(
+                    '.png') + description.count('.jpg') + description.count('.jpeg')
+    imagesInComments = sum(comment['node']['body'].count('.png') + comment['node']['body'].count(
+        '.jpg') + comment['node']['body'].count('.jpeg') for comment in comments)
+    totalImages = imagesInDescription + imagesInComments
+    participantNumber = issueData['participants']['totalCount']
 
-                    labels = []
-                    if issueData['labels'] is not None and issueData['labels']['edges'] is not None:
-                        for label in issueData['labels']['edges']:
-                            if label['node'] is None or label['node']['name'] is None:
-                                continue
-                            labels.append(str(label['node']['name']))
-
-                    # check if there is existing comment(s) for the issue
-                    for comment in comments:
-                        if comment is None or comment['node'] is None or comment['node']['author'] is None or comment['node']['author']['login'] is None:
-                            continue
-                        else:
-                            participantNumber.add(
-                                comment['node']['author']['login'])
-                    totalParticipants = 1 + len(participantNumber)
-
-                    # Append all the info to list
-                    issuesList.append((appName, issueTitle, issueLink, dateCreated, userCreated, status, labels, description,
-                                      comments, imagesInDescription, imagesInComments, totalImages, totalParticipants))
-            # error 403 means that the rate limit is exceeded, need to sleep for an hour
-            elif response.status_code == 403 or response.status_code == 429:
-                print(
-                    f"App {userName}/{reposName} error at issue: {currentIssueNum}")
-                print("status code is: " + str(response.status_code) +
-                      'rate limit exceeded or too many requests')
-                print(f'total issues that have collected {len(issuesList)}')
-
-                print("write to .csv file")
-                df = pd.DataFrame(issuesList, columns=['App Name', 'Issue Title', 'Issue Link', 'Date Created', 'User Created',
-                                                       'Status', 'labels', 'Description', 'Comments', 'Images In Description',
-                                                       'Images In Comments', 'Total Images', 'Total Participants'])
-                df.to_csv(f'{reposName} issues.csv', index=False)
-
-                time.sleep(3600)
-                currentIssueNum += 1
+    labels = []
+    if issueData['labels'] is not None and issueData['labels']['edges'] is not None:
+        for label in issueData['labels']['edges']:
+            if label['node'] is None or label['node']['name'] is None:
                 continue
-            else:
-                print(
-                    f"App {userName}/{reposName} error at issue: {currentIssueNum}")
-                print("status code is: " + str(response.status_code))
-                currentIssueNum += 1
-                continue
-            currentIssueNum += 1
-        except Exception:
-            df = pd.DataFrame(issuesList, columns=['App Name', 'Issue Title', 'Issue Link', 'Date Created', 'User Created',
-                                                   'Status', 'labels', 'Description', 'Comments', 'Images In Description',
-                                                   'Images In Comments', 'Total Images', 'Total Participants'])
-            df.to_csv(f'{reposName} issues.csv', index=False)
-
-            # log file to check special errors
-            issueLogFile = open(f'log/issues_log_{reposName}.txt', 'a')
-            issueLogFile.write(f'Error at issues: {currentIssueNum} \n')
-            currentIssueNum += 1
+            labels.append(str(label['node']['name']))
+    
+    cleanedComments = ''
+    for i, comment in enumerate(comments):
+        if comment is None or comment['node'] is None or comment['node']['body'] is None:
             continue
+        else:
+            cleanedComments += (f"######Comment {i}##########\n")
+            cleanedComments += (str(comment['node']['body']))
+    cleanedComments += (f"\n######End comments##########\n")
 
-    # Write the final state of issuesList to a CSV file
-    df = pd.DataFrame(issuesList, columns=['App Name', 'Issue Title', 'Issue Link', 'Date Created', 'User Created',
-                                           'Status', 'labels', 'Description', 'Comments', 'Images In Description',
-                                           'Images In Comments', 'Total Images', 'Total Participants'])
-    df.to_csv(f'{reposName} issues.csv', index=False)
+    totalParticipants = participantNumber
 
+    print(f'total participant: {totalParticipants}')
+    issuesList.append((reposName, issueNum, issueTitle, issueLink, dateCreated, userCreated, status, labels, description,
+                        cleanedComments, imagesInDescription, imagesInComments, totalImages, totalParticipants))
 
-if __name__ == '__main__':
-    args = parser.parse_args()
-    appDf = pd.read_csv(args.filePath)
-    for row in appDf.iterrows():
-        idx = row[0]
-        userAndReposName = appDf.iloc[idx]['html_url_clean']
-        end = userAndReposName.rfind('/')
-        userName = userAndReposName[:end]
-        reposName = userAndReposName[end+1:]
-
-        restHeader = {"Authorization": 'token ' +
-                      f"{args.restToken}"}
-
-        restUrl = f'https://api.github.com/repos/{userName}/{reposName}/issues'
-
-        restResponse = requests.get(restUrl, headers=restHeader)
-        try:
-            if restResponse.status_code == 200:
-                lastIssueNumUrl = restResponse.json()[0]['url']
-                lastIssueNum = lastIssueNumUrl[lastIssueNumUrl.rfind('/') + 1:]
-                issuesRetrieval(userName, reposName, lastIssueNum, args.graphQLToken)
-            else:
-                print( f'Error at {userName}/{reposName} with status: {restResponse.status_code}')
-                appLogFile = open('log/app_log_{reposName}.txt', 'a')
-                appLogFile.write(f'Error at app: {userName}/{reposName} with status: {restResponse.status_code}\n')
-                appLogFile = open('log/app_log_{reposName}.txt', 'a').close()
-                continue
-        except Exception as e:
-            appLogFile = open('log/app_log_{reposName}.txt', 'a')
-            print(f'Error at app: {userName}/{reposName} with exception: {e}')
-            appLogFile.write(f'Error at app: {userName}/{reposName} with exception: {e} \n')
-            appLogFile.close()
-            continue
+    toTXT(reposName, issueNum, issueTitle, description, cleanedComments)
